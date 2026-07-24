b7eb35d422ad117902598d48a4457f9d64e62c03
juanospina0409 <juanospina0409@hotmail.com>
2026-07-23 19:44:02 -0500

    chore: Se agrega un health check para los endpoints con el fin de mantener la sesion activa y evitar arranques en frío de Render

--- backend-django/inventario/urls.py
+++ backend-django/inventario/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmpresaViewSet, ProductoViewSet, registrar_usuario, login_usuario, obtener_perfil
+from .views import EmpresaViewSet, ProductoViewSet, registrar_usuario, login_usuario, obtener_perfil, health_check
 
 # El Router de DRF genera automáticamente las rutas estándar de tipo /empresas/, /empresas/id/
 router = DefaultRouter()
@@ -12,4 +12,5 @@ urlpatterns = [
     path('auth/registrar/', registrar_usuario, name='registrar_usuario'),
     path('auth/login/', login_usuario, name='login_usuario'),
     path('auth/me/', obtener_perfil, name='obtener_perfil'),
+    path('api/health/', health_check, name='health_check'),
 ]
